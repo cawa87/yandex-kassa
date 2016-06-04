8410c074765e24ceb22f5aa63ec5e7e3028d97c9
ALeksandr Zamiatin <cawa123@mail.ru>
2016-06-04 16:08:05 +0300

    namespace fix

--- src/YandexKassa/Controllers/PaymentController.php
+++ src/YandexKassa/Controllers/PaymentController.php
@@ -4,7 +4,9 @@ namespace CawaKharkov\YandexKassa\Controllers;
 
 
 use App\Http\Controllers\Controller;
-use CawaKharkov\LaravelBalance\Models\BalanceTransaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class PaymentController
@@ -12,8 +14,80 @@ use CawaKharkov\LaravelBalance\Models\BalanceTransaction;
  */
 class PaymentController extends Controller
 {
+
+
+    /**
+     * PaymentController constructor.
+     */
+    public function __construct()
+    {
+    }
+
     public function form()
     {
         return view(config('yandex_kassa.form_view'));
+    }
+
+    public function check(Request $request)
+    {
+        Config::set('laravel-debugbar::config.enabled', false);
+
+        Log::info('app.requests', ['request' => $request->all()]);
+        $transactionId = $request->get('invoiceId');
+        $action = $request->get('action');
+        $value = $request->get('orderSumAmount');
+        $orderSumCurrencyPaycash = $request->get('orderSumCurrencyPaycash');
+        $orderSumBankPaycash = $request->get('orderSumBankPaycash');
+        $customerNumber = $request->get('customerNumber');
+        $md5 = $request->get('md5');
+        $requestDatetime = $request->get('requestDatetime');
+
+        $hash = md5($action . ';' . $value .
+            ';' . $orderSumCurrencyPaycash . ';' .
+            $orderSumBankPaycash . ';' .
+            '116273' . ';' . $transactionId . ';'
+            . $_POST['customerNumber'] . ';' . 'eKa5vVhcoLGf');
+        if (strtolower($hash) != strtolower($md5)) {
+            $code = 1;
+        } else {
+            $code = 0;
+        }
+
+        Log::info('yandex_check_code', ['code' => $code]);
+
+        print '<?xml version="1.0" encoding="UTF-8"?>';
+        print '<checkOrderResponse performedDatetime="' .
+            $requestDatetime . '" code="' . $code . '"' .
+            ' invoiceId="' . $transactionId .
+            '" shopId="' . '116273' . '"/>';
+    }
+
+    public function aviso(Request $request)
+    {
+        Log::info('app.requests', ['request' => $request->all()]);
+
+        $transactionId = $request->get('invoiceId');
+        $action = $request->get('action');
+        $value = $request->get('orderSumAmount');
+        $orderSumCurrencyPaycash = $request->get('orderSumCurrencyPaycash');
+        $orderSumBankPaycash = $request->get('orderSumBankPaycash');
+        $customerNumber = $request->get('customerNumber');
+        $md5 = $request->get('md5');
+        $requestDatetime = $request->get('requestDatetime');
+
+        $hash = md5($action . ';' . $value . ';' . $orderSumCurrencyPaycash . ';'
+            . $orderSumBankPaycash . ';'
+            . '116273' . ';' . $transactionId
+            . ';' . $customerNumber . ';' . 'eKa5vVhcoLGf');
+        if (strtolower($hash) != strtolower($md5)) {
+            $code = 1;
+        } else {
+            $code = 0;
+        }
+        print '<?xml version="1.0" encoding="UTF-8"?>';
+        print '<paymentAvisoResponse performedDatetime="' .
+            $requestDatetime . '" code="' . $code .
+            '" invoiceId="' . $transactionId . '" shopId="' .
+            '116273' . '"/>';
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,15 @@
 <?php
 
-Route::group(['middleware' => 'auth','prefix' => config('yandex_kassa.prefix')], function () {
+Route::group(['prefix' => config('yandex_kassa.prefix')], function () {
     Route::get('form', [
         'as' => 'yandex_kassa.form',
         'uses' => '\CawaKharkov\YandexKassa\Controllers\PaymentController@form']);
+
+    Route::post('check', [
+        'as' => 'yandex_kassa.check',
+        'uses' => '\CawaKharkov\YandexKassa\Controllers\PaymentController@check']);
+    
+    Route::post('payment', [
+        'as' => 'yandex_kassa.aviso',
+        'uses' => '\CawaKharkov\YandexKassa\Controllers\PaymentController@aviso']);
 });

450c18f701324a57aa35c42869ab75098b0a4652
ALeksandr Zamiatin <cawa123@mail.ru>
2016-06-08 13:36:26 +0300

    payment page status

---
+++ src/YandexKassa/Controllers/PaymentPageController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CawaKharkov\YandexKassa\Controllers;
+
+
+use App\Http\Controllers\Controller;
+use CawaKharkov\YandexKassa\Interfaces\YandexPaymentRepositoryInterface;
+use CawaKharkov\LaravelBalance\Models\BalanceTransaction;
+use CawaKharkov\YandexKassa\Requests\PaymentRequest;
+use CawaKharkov\YandexKassa\Validators\PaymentValidator;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Response;
+
+
+/**
+ * Class PaymentPageController
+ * @package CawaKharkov\YandexKassa\Controllers
+ */
+class PaymentPageController extends Controller
+{
+
+    /**
+     * Success payment page
+     * @return mixed
+     */
+    public function success()
+    {
+        return view('yandex_kass::payment_page.success');
+    }
+
+    /**
+     * Faild payment page
+     * @return mixed
+     */
+    public function fail()
+    {
+        return view('yandex_kass::payment_page');
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -13,4 +13,15 @@ Route::group(['prefix' => config('yandex_kassa.prefix')], function () {
     Route::post('payment', [
         'as' => 'yandex_kassa.aviso',
         'uses' => '\CawaKharkov\YandexKassa\Controllers\PaymentController@aviso']);
+    
+       
+    Route::get('success', [
+        'as' => 'yandex_kassa.page.success',
+        'uses' => '\CawaKharkov\YandexKassa\Controllers\PaymentPageController@success']);
+
+    Route::get('fail', [
+        'as' => 'yandex_kassa.page.fail',
+        'uses' => '\CawaKharkov\YandexKassa\Controllers\PaymentPageController@fail']);
+
+    
 });

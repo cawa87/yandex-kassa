40b7dbdc6035d06ff20c35e2f231caad2b7a3c56
ALeksandr Zamiatin <cawa123@mail.ru>
2016-06-09 23:19:18 +0300

    event

---
+++ src/YandexKassa/Events/PaymentCreated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace YandexKassa\Events;
+
+use App\Podcast;
+use App\Events\Event;
+use CawaKharkov\YandexKassa\Interfaces\YandexPaymentInterface;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Class PaymentCreated
+ * @package YandexKassa\Events
+ */
+class PaymentCreated extends Event
+{
+    use SerializesModels;
+
+    public $payment;
+
+    /**
+     * Create a new event instance.
+     * @param YandexPaymentInterface $transaction
+     */
+    public function __construct(YandexPaymentInterface $payment)
+    {
+        $this->payment = $payment;
+    }
+}
